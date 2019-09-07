bcdc01e986211178e738623526de9c452bacc82e
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2019-09-07 22:29:06 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,11 +1,16 @@
 Event.handler('Controller.oncheck', function () {
 	if (Controller.store().counter === 1) return;
 	var layers = Controller.getWorkLayers();
+	var path = false;
 	Controller.run(layers, function (layer) {
 		if (!Event.fire('Layer.isshow', layer)) return;
 		if (layer.seojsontpl) layer.seojson = Template.parse([layer['seojsontpl']], layer);
 		if (!layer.seojson) return;
-		var seo = Load.loadJSON('-layer-seojson/?src='+encodeURIComponent(layer.seojson));
-		if (seo && seo.title) document.title = seo.title;
+		path = layer.seojson;
 	});
+
+	if (path) {
+		var seo = Load.loadJSON('-layer-seojson/?src='+encodeURIComponent(encodeURIComponent(path)));
+		if (seo && seo.title) document.title = seo.title;	
+	}
 });
